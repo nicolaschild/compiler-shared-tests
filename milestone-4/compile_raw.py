4f125896dabbd751b6014b69926aed6d8b419e90
Nicolas Child <nick@zenpayments.com>
2024-04-06 14:22:46 -0600

    Fixing things up

--- milestone-4/compile_raw.py
+++ milestone-4/compile_raw.py
@@ -6,8 +6,8 @@ tests = [
             cout << c;
             }
         ---
-        2
-            """,
+        7
+    """,
     """
         void main(){
             int c = 2;
@@ -15,8 +15,8 @@ tests = [
             cout << c;
             }
         ---
-        2
-            """,
+        3
+    """,
     """
         void main(){
             int c = 2;
@@ -25,7 +25,7 @@ tests = [
             }
         ---
         3
-            """,
+    """,
     """
         void main(){
             int c = 2;
@@ -34,7 +34,7 @@ tests = [
             }
         ---
         2
-            """,
+    """,
     """
         void main(){
             int c = 2;
@@ -43,7 +43,7 @@ tests = [
             }
         ---
         4
-        """,
+    """,
     """
         void main(){
             int n = 3;
@@ -53,8 +53,8 @@ tests = [
             cout << t;
             }
         ---
-        45
-        """,
+        43
+    """,
     """
     void main(){
         int n = 3;
@@ -65,7 +65,7 @@ tests = [
         }
     ---
     43
-        """,
+    """,
     """
         void main(){
             int n = 1;
@@ -84,7 +84,7 @@ tests = [
             }
         ---
         one
-        """,
+    """,
     """
         void main(){
             int n = 2;
@@ -103,7 +103,7 @@ tests = [
             }
         ---
         two or three
-        """,
+    """,
     """
         void main(){
             int n = 0;
@@ -122,7 +122,7 @@ tests = [
             }
         ---
         a number not in the range of 1 to 3
-        """,
+    """,
     """
         void main(){
             int n = 2;
@@ -151,328 +151,348 @@ tests = [
             }
         ---
         two
-            """,
+    """,
     """
-        void main(){
-        if(false){cout << "upper";} else {cout << "lower";}}
-        ---
-        lower
-            """,
+    void main(){
+        if (false) {
+            cout << "upper";
+        } 
+        else {
+            cout << "lower";
+        }
+    }
+    ---
+    lower
+    """,
     """
-        void main(){
+    void main() {
         bool x = 1 < 5 || false;
-        if(x){cout << "upper";} else {cout << "lower";}}
-        ---
-        upper
-            """,
+        if(x) {
+            cout << "upper";
+        }
+        else {
+            cout << "lower";
+        }
+    }
+    ---
+    upper
+    """,
     """
-        void main(){
+    void main() {
         int x = 10;
-        while(x > 0){
+        while(x > 0) {
             cout << x;
             x = x - 1;
         }
-        }
-        ---
-        10987654321
-            """,
+    }
+    ---
+    10987654321
+    """,
     """
-        void main(){
+    void main() {
         int x = 10;
-        while(x > 0){
+        while (x > 0){
             cout << x;
+            //RHS should never evaluate, if it does, this is an infinite loop (you're welcome if this broke lol)
             true || x < (x += 10);
             x = x - 1;
         }
-        }
-        ---
-        10987654321
-        """,
+    }
+    ---
+    10987654321
+    """,
     """
     void main() {
-    cout << "Hello World!";
-}
----
-""",
+        cout << "Hello World!";
+    }
+    ---
+    Hello World!
+    """,
     """
-    void main(){
-    int n = 1;
-    switch n {
-        case 1:
-            cout << "one";
-            break;
-        case 2:
-        case 3:
-            cout << "two or three";
-            break;
-        default:
-            cout << "a number not in the range of 1 to 3";
-            break;
-        }
-}
----
-""",
-    """
-void main() {
-    int x = 0;
-    if(1 < x){
+    void main() {
+        int x = 0;
+        if(1 < x){
             cout << "This should not print";
+        } else {
+            cout << "This should print";
+        }
+    }
+    ---
+    This should print
+    """,
+    """
+    void main() {
+        int x = 0;
+        if(x < 1) {
+            cout << "This should print";
+        }
+        else {
+            cout << "This should not print";
+        }
+    }
+    ---
+    This should print
+    """,
+    """
+    void main() {
+        int x = 0;
+        if (x > 1) {
+            cout << "This should not print";
+        }
+        else {
+            cout << "This should print";
+        }   
+    }
+    ---
+    This should print
+    """,
+    """
+    void main() {
+        int x = 0;
+        if (1 > x) {
+            cout << "This should print";
+        }
+        else {
+            cout << "This should not print";
+        }
+    }
+    ---
+    This should print
+    """,
+    """
+    void main() {
+        int x = 0;
+        if(x == 0) {
+            cout << "This should print";
+        }
+        else {
+            cout << "This should not print";
+        }
     }   
-}
----
-""",
+    ---
+    This should print
+    """,
     """
-void main() {
-    int x = 0;
-    if(x < 1){
-            cout << "This should print";
-    }
-    else {
-            cout << "This should not print";
-    }
-}
----
-""",
-    """
-void main() {
-    int x = 0;
-    if(x > 1){
-            cout << "This should not print";
-    }
-    else {
-            cout << "This should print";
-    }   
-}
----
-""",
-    """
-void main() {
-    int x = 0;
-    if(1 > x){
-            cout << "This should print";
-    }
-    else {
-            cout << "This should not print";
-    }
-}
----
-""",
-    """
-void main() {
-    int x = 0;
-    if(x == 0){
-        cout << "This should print";
-    }
-    else {
-        cout << "This should not print";
-    }
-}
----
-""",
-    """
-void main() {
-    int x = 1;
-    if(0 == x){
-        cout << "This should not print";
-    }
-    else {
-        cout << "This should print";
-    }
-}
----
-""",
-    """
-void main() {
-    int x = 0;
-    if(x != 1){
-        cout << "This should print";
-    }
-    else {
-        cout << "This should not print";
-    }
-}
----
-""",
-    """
-void main() {
-    int x = 1;
-    if(1 != x){
-        cout << "This should not print";
-    }
-    else {
-        cout << "This should print";
-    }
-}
----
-""",
-    """
-void main() {
-    int x = 0;
-    if(x == 0){
-        if(x == 0){
-            cout << "This should print";
-        }
-    }
-}
----
-""",
-    """
-void main() {
-    int x = 0;
-    if(x == 0){
-        if(x == 0){
-            if(x == 0){
-                cout << "This should print";
-            }
-        }
-    }
-}
----
-""",
-    """
-void main() {
-    int x = 0;
-    if(x != 0){
-        cout << "This should not print";
-    }
-    else {
-        if(x == 0){
-            cout << "This should print";
-        }
-    }
-}
----
-""",
-    """
-void main() {
-    int x = 0;
-    if(x != 0){
-        cout << "This should not print";
-    }
-    else {
-        if(x != 0){
+    void main() {
+        int x = 1;
+        if (0 == x) {
             cout << "This should not print";
         }
         else {
             cout << "This should print";
         }
     }
-}
----
-""",
+    ---
+    This should print
+    """,
     """
-void main() {
-    int i = 0;
-    for(i = 0; i < 5; i = i + 1){
-        cout << i;
-    }
-}
----
-""",
-    """
-void main() {
-    int i = 0;
-    for(; i < 5; i = i + 1){
-        cout << i;
-    }
-}
----
-""",
-    """
-void main() {
-    int i = 0;
-    for(; i < 5;){
-        cout << i;
-        i = i + 1;
-    }
-}
----
-""",
-    """
-void main() {
-    int i = 0;
-    for(; i < 5;){
-        cout << i;
-        i = i + 1;
-        if(i == 3){
-            break;
+    void main() {
+        int x = 0;
+        if (x != 1) {
+            cout << "This should print";
+        }
+        else {
+            cout << "This should not print";
         }
     }
-}
----
-""",
+    ---
+    This should print
+    """,
     """
-void main() {
-    int i = 0;
-    for(; i < 3; i = i + 1){
-        int j = 0;
-        for(; j < 3; j = j + 1){
-            cout << i;
-            cout << j;
+    void main() {
+        int x = 1;
+        if (1 != x) {
+            cout << "This should not print";
+        }
+        else {
+            cout << "This should print";
         }
     }
-}
----
-""",
+    ---
+    This should print
+    """,
     """
-void main() {
-    int i = 0;
-    for(; i < 3; i = i + 1){
-        int j = 0;
-        for(; j < 3; j = j + 1){
+    void main() {
+        int x = 0;
+        if (x == 0) {
+            if (x == 0) {
+                cout << "This should print";
+            }
+        }
+    }
+    ---
+    This should print
+    """,
+    """
+    void main() {
+        int x = 0;
+        if (x == 0) {
+           if (x == 0) {
+                if (x == 0) {
+                    cout << "This should print";
+                }
+            }
+        }
+    }
+    ---
+    This should print
+    """,
+    """
+    void main() {
+        int x = 0;
+        if (x != 0) {
+            cout << "This should not print";
+        }
+        else {
+            if(x == 0) {
+               cout << "This should print";
+            }
+        }
+    }
+    ---
+    This should print
+    """,
+    """
+    void main() {
+        int x = 0;
+        if  (x != 0) {
+            cout << "This should not print";
+        }
+        else {
+            if (x != 0) {
+                cout << "This should not print";
+            }
+            else {
+                cout << "This should print";
+            }
+        }
+    }
+    ---
+    This should print
+    """,
+    """
+    void main() {
+        int i = 0;
+        for (i = 0; i < 5; i = i + 1) {
             cout << i;
-            cout << j;
-            if(j == 1){
+        }
+    }
+    ---
+    01234
+    """,
+    """
+    void main() {
+        int i = 0;
+        for(; i < 5; i = i + 1){
+            cout << i;
+        }
+    }
+    ---
+    """,
+    """
+    void main() {
+        int i = 0;
+        for (; i < 5;) {
+            cout << i;
+            i = i + 1;
+        }
+    }
+    ---
+
+    """,
+    """
+    void main() {
+        int i = 0;
+        for(; i < 5;){
+            cout << i;
+            i = i + 1;
+            if (i == 3) {
                 break;
             }
         }
     }
-}
----
-""",
+    ---
+    012
+    """,
     """
-void main() {
-    int i = 0;
-    while  i < 5  {
-        cout << i;
-        i = i + 1;
-    }
-}
----
-""",
-    """
-void main() {
-    int i = 0;
-    while  i < 3  {
-        int j = 0;
-        while  j < 3  {
-            cout << i;
-            cout << j;
-            j = j + 1;
-        }
-        i = i + 1;
-    }
-}
----
-""",
-    """
-void main() {
-    int i = 0;
-    while  i < 3  {
-        int j = 0;
-        while  j < 3  {
-            cout << i;
-            cout << j;
-            j = j + 1;
-            if(j == 1){
-                break;
+    //00 01 02 10 12 20 21 22
+    void main() {
+        int i = 0;
+        for(; i < 3; i = i + 1){
+            int j = 0;
+            for(; j < 3; j = j + 1){
+                cout << i;
+                cout << j;
             }
         }
-        i = i + 1;
     }
-}
----
-""",# TODO: add expected
+    ---
+    000102101112202122
+    """,
+    """
+    //00 01 10 11 20 21
+    void main() {
+        int i = 0;
+        for (; i < 3; i = i + 1) {
+            int j = 0;
+            for (; j < 3; j = j + 1) {
+                cout << i;
+                cout << j;
+                if (j == 1) {
+                    break;
+                }
+            }
+        }
+    }
+    ---
+    000110112021
+    """,
+    """
+    void main() {
+        int i = 0;
+        while (i < 5)  {
+            cout << i;
+            i = i + 1;
+        }
+    }
+    ---
+    01234
+    """,
+    """
+    // 00 01 02 10 11 12 20 21 22
+    void main() {
+        int i = 0;
+        while (i < 3)  {
+            int j = 0;
+            while  (j < 3)  {
+                cout << i;
+                cout << j;
+                j = j + 1;
+            }
+            i = i + 1;
+        }
+    }
+    ---
+    000102101112202122
+    """,
+    """
+    void main() {
+        int i = 0;
+        while  i < 3  {
+            int j = 0;
+            while  j < 3  {
+                cout << i;
+                cout << j;
+                j = j + 1;
+                if(j == 1){
+                    break;
+                }
+            }
+            i = i + 1;
+        }
+    }
+    ---
+    """,
     """
     void main() {
     int x = 1;
